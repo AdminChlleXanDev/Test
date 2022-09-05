50e8e3013d4b19269b776921e6c03ecee1b7d697
ChlleXanDev <108337657+ChlleXanDev@users.noreply.github.com>
2022-09-05 14:46:42 +0700

    test

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+    function openNav() {
+        document.getElementById("sidebar").style.width = "250px";
+        document.getElementById("nav-list").style.marginLeft = "250px";
+    }
+    function closeNav() {
+        document.getElementById("sidebar").style.width = "0";
+        document.getElementById("nav-list").style.marginLeft = "0";
+    }
+</script>
